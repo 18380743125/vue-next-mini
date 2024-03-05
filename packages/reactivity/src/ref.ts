d38c915a803dd906e15d1469f2b2192eb5963f72
tangl <2505044623@qq.com>
2024-03-05 22:31:56 +0800

    add：ref

---
+++ packages/reactivity/src/ref.ts
@@ -0,0 +1,74 @@
+import { hasChanged } from '@vue/shared'
+import { createDep, Dep } from './dep'
+import { toReactive } from './reactive'
+import { activeEffect, trackEffects, triggerEffects } from './effect'
+
+export interface Ref<T = any> {
+  value: T
+}
+
+export function ref(value?: unknown) {
+  return createRef(value, false)
+}
+
+function createRef(rawValue: unknown, shallow: boolean) {
+  if (isRef(rawValue)) {
+    return rawValue
+  }
+  return new RefImpl(rawValue, shallow)
+}
+
+class RefImpl<T> {
+
+  private _rawValue: T
+
+  private _value: T
+
+  public dep?: Dep = undefined
+
+  public readonly __v_isRef = true
+
+  constructor(value: T, public readonly __v_isShallow: boolean) {
+    this._rawValue = value
+    this._value = __v_isShallow ? value : toReactive(value)
+  }
+
+  get value() {
+    trackRefValue(this)
+    return this._value
+  }
+
+  set value(newVal) {
+    if (hasChanged(newVal, this._rawValue)) {
+      this._rawValue = newVal
+      this._value = toReactive(newVal)
+      triggerRefValue(this)
+    }
+  }
+}
+
+/**
+ * 触发依赖
+ */
+export function triggerRefValue(ref: RefImpl<any>) {
+  if (ref.dep) {
+    triggerEffects(ref.dep)
+  }
+}
+
+/**
+ * 收集依赖
+ */
+export function trackRefValue(ref: RefImpl<any>) {
+  if (activeEffect) {
+    trackEffects(ref.dep || (ref.dep = createDep()))
+  }
+}
+
+/**
+ * 是否为 ref
+ * @param r
+ */
+export function isRef(r: any): r is Ref {
+  return !!(r && r.__v_isRef)
+}
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,3 +2,10 @@
  * 判断是否为一个数组
  */
 export const isArray = Array.isArray
+
+/**
+ * 判断是否为一个对象
+ */
+export const isObject = (val: unknown) => val !== null && typeof val === 'object'
+
+export const hasChanged = (value: any, oldValue: any): boolean => !Object.is(value, oldValue)
