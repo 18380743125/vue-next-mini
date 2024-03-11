6fc3cba5afb1b0317b42ac372c9450d5e5b467ed
tangl <2505044623@qq.com>
2024-03-11 16:25:40 +0800

    update: 添加注释

--- packages/runtime-core/src/apiWatch.ts
+++ packages/runtime-core/src/apiWatch.ts
@@ -1,35 +1,52 @@
-import { isReactive } from '../../reactivity/src/reactive'
-import { queuePreFlushCb } from './scheduler'
-import { ReactiveEffect } from '../../reactivity/src/effect'
 import { hasChanged, isObject } from '@vue/shared'
+import { queuePreFlushCb } from './scheduler'
+import { isReactive } from '../../reactivity/src/reactive'
+import { ReactiveEffect } from '../../reactivity/src/effect'
 
-
+/**
+ * watch 配置项属性
+ */
 export interface WatchOptions<immediate = boolean> {
   immediate?: immediate
   deep?: boolean
 }
 
-export function watch(source: any, cb: (...args: any[]) => void, options?: WatchOptions) {
+/**
+ * 指定的 watch 函数
+ * @param source 监听的响应式数据
+ * @param cb 回调函数
+ * @param options 配置对象
+ * @returns
+ */
+export function watch(source: any, cb: Function, options?: WatchOptions) {
   return doWatch(source, cb, options)
 }
 
-function doWatch(source: any, cb: (...args: any[]) => void, { immediate, deep }: WatchOptions = {}) {
+function doWatch(
+  source: any,
+  cb: Function,
+  { immediate, deep }: WatchOptions = {}
+) {
+  // 触发 getter 的指定函数
   let getter: () => any
+  // 判断 source 的数据类型
   if (isReactive(source)) {
+    // 指定 getter
     getter = () => source
     deep = true
   } else {
-    getter = () => {
-    }
+    getter = () => {}
   }
 
+  // 存在回调函数和 deep
   if (!!cb && deep) {
     const baseGetter = getter
     getter = () => traverse(baseGetter())
   }
 
+  // 旧值
   let oldValue = {}
-
+  // job 执行方法
   const job = () => {
     if (!!cb) {
       const newValue = effect.run()
@@ -39,6 +56,7 @@ function doWatch(source: any, cb: (...args: any[]) => void, { immediate, deep }:
     }
   }
 
+  // 调度器
   let scheduler = () => queuePreFlushCb(job)
 
   const effect = new ReactiveEffect(getter, scheduler)
@@ -57,6 +75,7 @@ function doWatch(source: any, cb: (...args: any[]) => void, { immediate, deep }:
   }
 }
 
+// 依次执行 getter，从而触发依赖收集
 export function traverse(value: unknown) {
   if (!isObject(value)) {
     return value
